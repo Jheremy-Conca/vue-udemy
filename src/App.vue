eaebb620e26993dada9ffdbcc0f9d17bc3fa7ae7
ConcaJheremy <concajheremy@gmail.com>
2025-01-20 14:56:42 -0500

    mi primer cmit vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+const name = "Vue 3";
+const activo = true;
+const arrayFrutas = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+    stock: 0,
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+    stock: 10,
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+    stock: 20,
+  },
+];
+</script>
+<template>
+  <div>
+    <h1>Hola {{ name.toUpperCase() }}</h1>
+    <ul>
+      <template v-for="item in arrayFrutas" :key="item.name">
+        <li v-if="item.stock > 0">{{ item.name }} - {{ item.price }}</li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
